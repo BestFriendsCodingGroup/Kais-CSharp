78ec5ad8081a5f0c3df710cdcf34127b93ba6aea
KaiCantCode <125975816+KaiCantCode@users.noreply.github.com>
2023-03-22 09:59:11 +0000

    C# kai

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+// See https://aka.ms/new-console-template for more information
+Console.Title = "SkyNet";
+Console.ForegroundColor = ConsoleColor.Green;
+Console.BackgroundColor = ConsoleColor.Blue;
+Console.Clear();
 Console.WriteLine("Hello, World!");
+
+Console.ReadKey();
+Console.ResetColor();
+Console.Clear();
